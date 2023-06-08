7ae50bd8b09726a21e4257fcbd4aec73961c7264
Sayed Hossain Bhuiyan <mdshiam4@gmail.com>
2023-06-09 02:39:33 +0600

    home page is done

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import ExtraSection from "../ExtraSection/ExtraSection";
 import OurClass from "../OurClass/OurClass";
+import OurInstructors from "../OurInstructors/OurInstructors";
 import Slider from "../Slider/Slider";
 
 
@@ -8,6 +9,7 @@ const Home = () => {
         <div>
             <Slider></Slider>
             <OurClass></OurClass>
+            <OurInstructors></OurInstructors>
             <ExtraSection></ExtraSection>
         </div>
     );
