9b3d551b6d500a716baea0d4f11130c09e103d74
Sayed Hossain Bhuiyan <mdshiam4@gmail.com>
2023-06-12 00:22:44 +0600

    delete items is done

--- src/Pages/Dashboard/MyCart/MyCart.jsx
+++ src/Pages/Dashboard/MyCart/MyCart.jsx
@@ -1,17 +1,41 @@
 import { Link } from "react-router-dom";
 import useClass from "../../../hooks/useClass";
 import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 
 const MyCart = () => {
-    const [items,] = useClass();
+    const [items,refetch] = useClass();
     // console.log(items);
-    
-    // how does reduce work!!!
     const total = items.reduce((sum, item) => item.price + sum, 0);
 
     const handleDelete=item=>{
-        console.log(item);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/items/${item._id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
     }
     return (
         <div className="w-full">
